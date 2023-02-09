a19c86f26c6e24204b1c8071a8829929c050db28
Cameron McEvenue <mcevenc@gmail.com>
2023-02-09 14:37:08 -0500

    Cloned widget-boilerplate

---
+++ meeting-scheduler/src/App.svelte
@@ -0,0 +1,17 @@
+<script>
+  import { onMount } from "svelte";
+  import "./app.css";
+
+  onMount(() => {
+    ZOHO.embeddedApp.init();
+  });
+</script>
+
+<svelte:head>
+  <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
+  <script
+    src="https://live.zwidgets.com/js-sdk/1.1/ZohoEmbededAppSDK.min.js"
+  ></script>
+</svelte:head>
+
+<h1>Svelte + Tailwind CSS + Zoho SDK Skeleton</h1>
