8635f0dcdd4845d95836b91bd1edbdcab75347a3
Cameron McEvenue <mcevenc@gmail.com>
2023-02-09 15:16:39 -0500

    Added Cal.com embed code + CallScheduler component

--- widgets/meeting-scheduler/src/App.svelte
+++ widgets/meeting-scheduler/src/App.svelte
@@ -1,19 +1,55 @@
 <script>
   import { onMount } from "svelte";
   import "./app.css";
-  import Tester from "@shared/components/Tester.svelte";
+
+  import CallScheduler from "./components/CalllScheduler.svelte";
 
   onMount(() => {
-    ZOHO.embeddedApp.init();
+    //ZOHO.embeddedApp.init();
   });
 </script>
 
 <svelte:head>
+  <!-- Fonts -->
   <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
+  <!--Zoho SDK-->
   <script
     src="https://live.zwidgets.com/js-sdk/1.1/ZohoEmbededAppSDK.min.js"
   ></script>
+  <!--Cal.com Embed Snippet-->
+  <script>
+    (function (C, A, L) {
+      let p = function (a, ar) {
+        a.q.push(ar);
+      };
+      let d = C.document;
+      C.Cal =
+        C.Cal ||
+        function () {
+          let cal = C.Cal;
+          let ar = arguments;
+          if (!cal.loaded) {
+            cal.ns = {};
+            cal.q = cal.q || [];
+            d.head.appendChild(d.createElement("script")).src = A;
+            cal.loaded = true;
+          }
+          if (ar[0] === L) {
+            const api = function () {
+              p(api, arguments);
+            };
+            const namespace = ar[1];
+            api.q = api.q || [];
+            typeof namespace === "string"
+              ? (cal.ns[namespace] = api) && p(api, ar)
+              : p(cal, ar);
+            return;
+          }
+          p(cal, ar);
+        };
+    })(window, "https://cal.com/embed.js", "init");
+    Cal("init");
+  </script>
 </svelte:head>
 
-<h1>Svelte + Tailwind CSS + Zoho SDK Skeleton</h1>
-<Tester />
+<CallScheduler name="Cameron" email="cameron@cameronmcevenue.com" />
---
+++ widgets/meeting-scheduler/src/components/CalllScheduler.svelte
@@ -0,0 +1,27 @@
+<script>
+  import { onMount } from "svelte";
+
+  // Props
+
+  export let name;
+  export let email;
+  export let guests = [];
+  export let calLink = "aimeefairweatherco";
+
+  let theme = "light";
+
+  onMount(() => {
+    Cal("inline", {
+      elementOrSelector: "#Cal", // You can also provide an element directly
+      calLink, // The link that you want to embed. It would open https://cal.com/jane in embed
+      config: {
+        name, // Prefill Name
+        email, // Prefill Email
+        guests, // Prefill Guests
+        theme, // "dark" or "light" theme
+      },
+    });
+  });
+</script>
+
+<div id="Cal" />
